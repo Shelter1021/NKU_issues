35fc0a5a8fed77ed375028f45c8adb6fd91acf1f
Shelter <2338485672@qq.com>
2026-09-07 15:13:13 +0800

    Delete 2310422-谢小珂-RNN/2310422-谢小珂-RNN directory

--- 2310422-谢小珂-RNN/2310422-谢小珂-RNN/name_rnn_lstm_experiment/run_full_experiment.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python run_experiment.py --epochs 50 --hidden-size 128 --batch-size 128 --torch-threads 1
--- 2310422-谢小珂-RNN/2310422-谢小珂-RNN/name_rnn_lstm_experiment/src/name_rnn_lstm_experiment/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Character-level RNN/LSTM name classification experiment."""
-
-__all__ = ["data_utils", "models", "train", "plotting", "metrics"]
